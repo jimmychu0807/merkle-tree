54df8b914c7d1b6a5af2a8ba9cbe808180b6dda7
Jimmy Chu <jimmychu0807@gmail.com>
2022-09-25 23:18:14 +0800

    can calc merkle_root

--- docs/notes.md
+++ docs/notes.md
@@ -2,3 +2,7 @@
 
 - implement the merkle tree
 - publish the crate
+
+## Tasks:
+
+- convert from a vector of bytes to a displayable hex value need some conversion :) All conversion in Rust need some energy to be expend
--- examples/main.rs
+++ examples/main.rs
@@ -10,6 +10,15 @@ fn main() {
   let data4 = vec![b"abc", b"bcd", b"cde", b"def", b"efg"];
 
   let merkle_tree = MerkleTree::new();
+  let root1 = merkle_tree.merkle_root(data1.iter());
+  println!("merkle root: {:?}", hex::encode(&root1));
 
-  println!("{:?}", merkle_tree.merkle_root(data1.iter()));
+  let root2 = merkle_tree.merkle_root(data2.iter());
+  println!("merkle root: {:?}", hex::encode(&root2));
+
+  let root3 = merkle_tree.merkle_root(data3.iter());
+  println!("merkle root: {:?}", hex::encode(&root3));
+
+  let root4 = merkle_tree.merkle_root(data4.iter());
+  println!("merkle root: {:?}", hex::encode(&root4));
 }
