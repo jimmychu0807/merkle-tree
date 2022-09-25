f686b85e84a9bcb199efcceb791ed6ed6dc64161
Jimmy Chu <jimmychu0807@gmail.com>
2022-09-25 23:29:02 +0800

    Initial impl of merkle_root (#1)
    * updated
    
    * wip
    
    * a merkle tree with no custom hasher
    
    * can calc merkle_root

--- examples/main.rs
+++ examples/main.rs
@@ -1,10 +1,24 @@
 extern crate merkle_tree;
 
-pub use merkle_tree::{MerkleTree};
+use merkle_tree::{ MerkleTree };
+// use blake2::Blake2s256;
 
 fn main() {
-  let data = [b"abc"];
+  let data1 = vec![b"abc"];
+  let data2 = vec![b"abc", b"bcd"];
+  let data3 = vec![b"abc", b"bcd", b"cde"];
+  let data4 = vec![b"abc", b"bcd", b"cde", b"def", b"efg"];
 
-  println!("Hello World!");
-  println!("{:?}", MerkleTree::merkle_root(data.iter()));
+  let merkle_tree = MerkleTree::new();
+  let root1 = merkle_tree.merkle_root(data1.iter());
+  println!("merkle root: {:?}", hex::encode(&root1));
+
+  let root2 = merkle_tree.merkle_root(data2.iter());
+  println!("merkle root: {:?}", hex::encode(&root2));
+
+  let root3 = merkle_tree.merkle_root(data3.iter());
+  println!("merkle root: {:?}", hex::encode(&root3));
+
+  let root4 = merkle_tree.merkle_root(data4.iter());
+  println!("merkle root: {:?}", hex::encode(&root4));
 }
